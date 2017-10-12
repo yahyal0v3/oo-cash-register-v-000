d513acfa9ed1ce5ace9e3c3c2a180f53d71b8d2f
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-12 18:54:30 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :discount, :total
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @cart = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    @cart += [title] * quantity
+    @total += price * quantity
+  end
+
+  def apply_discount
+    if @discount > 0
+      amount_off = @total * @discount / 100.0
+      @total -= amount_off
+      "After the discount, the total comes to $#{total.floor}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    @cart
+  end
+
+end
